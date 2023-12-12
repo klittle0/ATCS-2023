65b3660e853ffbc1873833d448c20472f35c7c43
kate <kate.little@menloschool.org>
2023-12-12 08:18:45 -0800

    12/12 - updated graphics + start of FSM

--- FinalProject/map.py
+++ FinalProject/map.py
@@ -10,13 +10,18 @@ WHITE = (255, 255, 255)
 GRAY = (211, 211, 211)
 RED = (255, 0, 0)
 FPS = 60
+filepath = "images/"
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, width, height):
         self.x = width // 2
         self.y = height // 2
-        self.radius = PERSON_SIZE // 2
+        self.width = 34
+        self.height = 43
         self.speed = 0.8
+        self.image = pygame.image.load(filepath+"dora.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (width // 2, height // 2)  # Set the center of the image
 
     def move(self, keys, map_width, map_height):
         if keys[pygame.K_LEFT] and self.x - self.speed > -map_width // 2:
@@ -35,9 +40,14 @@ class Obstacle(pygame.sprite.Sprite):
         self.y = random.randint(-map_height // 2, map_height // 2)
         # make it so that it moves at player's speed
         self.speed = 0.7  # Initial speed
+        self.images = [pygame.image.load(filepath+"suitcase.png"), pygame.image.load(filepath+"tsa_officer.png")]
+        self.image = random.choice(self.images)  # Randomly choose image
+        # Set dimensions 
+        self.width, self.height = 25, 40
 
     def draw(self, screen, player_x, player_y):
-        pygame.draw.circle(screen, RED, (self.x + WIDTH // 2 - player_x, self.y + HEIGHT // 2 - player_y), PERSON_SIZE // 2)
+        screen.blit(self.image, (self.x + WIDTH // 2 - player_x - self.width // 2, self.y + HEIGHT // 2 - player_y - self.height // 2))
+
 
     def chase_player(self, player):
         dx = player.x - self.x
@@ -104,7 +114,6 @@ class Game:
             self.player.move(keys, MAP_WIDTH, MAP_HEIGHT)
 
             # Move and draw obstacles
-            print(self.player.speed)
             for obstacle in self.obstacles:
                 obstacle.chase_player(self.player)
                 obstacle.draw(self.screen, self.player.x, self.player.y)
@@ -113,9 +122,9 @@ class Game:
                 obstacle_x, obstacle_y = obstacle.x, obstacle.y
                 distance = math.sqrt((obstacle_x - self.player.x) ** 2 + (obstacle_y - self.player.y) ** 2)
 
-                if distance <= (self.player.radius + PERSON_SIZE // 2):
+                if distance <= (self.player.width + obstacle.width // 2):
                     self.obstacles.remove(obstacle)
-                    self.player.speed -= 0.2  # Decrease player speed when hitting an obstacle
+                    self.player.speed -= 0.35  # Decrease player speed when hitting an obstacle
 
              # Iterate over food items with index for accurate removal
             index = 0
@@ -127,7 +136,7 @@ class Game:
                 distance = math.sqrt((food_x - self.player.x) ** 2 + (food_y - self.player.y) ** 2)
 
                 # Check collision between player and food
-                if distance <= (self.player.radius + FOOD_SIZE // 2):
+                if distance <= (self.player.width + FOOD_SIZE // 2):
                     self.foods.pop(index)
                     self.player.speed += 0.1  # Increase player speed slightly when eating food
                 else:
@@ -135,10 +144,8 @@ class Game:
                     pygame.draw.circle(self.screen, RED, (food_x + WIDTH // 2 - self.player.x, food_y + HEIGHT // 2 - self.player.y), FOOD_SIZE // 2)
                     index += 1  # Move to the next food item if no collision
 
-            pygame.draw.circle(self.screen, RED, (WIDTH // 2, HEIGHT // 2), self.player.radius)
-
-
-            pygame.draw.circle(self.screen, RED, (WIDTH // 2, HEIGHT // 2), self.player.radius)
+            # Draw player 
+            self.screen.blit(self.player.image, self.player.rect)
 
             pygame.display.flip()
             self.clock.tick(FPS)
